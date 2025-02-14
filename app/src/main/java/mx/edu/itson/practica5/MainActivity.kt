525e35ba73a34a29ddd5acf9f6292ba2da55eb64
bortcrab <bortcrab@gmail.com>
2025-02-13 23:56:16 -0700

    Terminé el front

---
+++ app/src/main/java/mx/edu/itson/practica5/MainActivity.kt
@@ -0,0 +1,30 @@
+package mx.edu.itson.practica5
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        val button:Button = findViewById(R.id.btnGetStarted)
+
+        button.setOnClickListener {
+            var intent:Intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
